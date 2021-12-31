b3de175d7e1315a7991b6b8eca941a0a8806debe
tdltdc <me@oraetlabora.club>
2021-12-31 14:56:13 +0100

    Change filename and working directory name to match Github repository name

--- sbsc.py
+++ sbsc.py
@@ -36,7 +36,7 @@ while True:
         time.sleep(1)
 
 # Wijzig werkmap
-os.chdir('/home/pi/youtube_synchronisatiecontrole/')
+os.chdir('/home/pi/sbsc/')
 
 # Haal lijst met video ids van geplande uitzendingen op
 upcoming_broadcasts_list_url = 'https://youtube.googleapis.com/youtube/v3/search?part=id&channelId={}&eventType=upcoming&maxResults=1000&order=date&type=video&key={}'.format(channel_id, api_key)
